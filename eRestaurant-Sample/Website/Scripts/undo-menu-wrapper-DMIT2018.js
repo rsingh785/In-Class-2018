bd4c8dcda0c905a6a8e0036c955a8db6528f26ec
rsingh785 <rsingh47@studentmail.nait.ca>
2014-10-02 08:29:38 -0600

    Added Files

--- eRestaurant-Sample/Website/Scripts/undo-menu-wrapper-DMIT2018.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="jquery-1.10.2.js" />
-/// <reference path="bootstrap.js" />
-
-/* Remove float styling that was injected for the menu control */
-
-$(function () {
-    // Isolate the context for applying Bootstrap styling
-    $('#SiteNav').next().removeAttr('style');
-});
