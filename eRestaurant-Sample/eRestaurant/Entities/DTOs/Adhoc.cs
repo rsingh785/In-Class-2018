bbd4df93b57b18cc689378cc1e0249a95bc6dab2
rsingh785 <rsingh47@studentmail.nait.ca>
2014-10-14 14:19:14 -0600

    CreatedNew Classes

---
+++ eRestaurant-Sample/eRestaurant/Entities/DTOs/Adhoc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities.DTOs
+
+{
+    
+     public class Adhoc :IAdhoc // This class implements IAdhoc interface
+    {
+         // A class has fields , properties , constructors and methods
+
+    }
+    public interface IAdhoc
+    {
+// An Interface has properties and methods
+        string name;
+        //  Properties and methods do Not have an implementation in an interface
+        string AsText(int value);
+        int Count { get; } // Properties can have a get , set or both
+
+    }
+}
