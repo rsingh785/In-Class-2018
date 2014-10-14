6ccc699050efba0de9d93babb4ac2f1eb4ffd1d0
rsingh785 <rsingh47@studentmail.nait.ca>
2014-10-14 13:21:43 -0600

    bLL EDITED

--- eRestaurant-Sample/Menu Category by strongly typed.linq
+++ eRestaurant-Sample/Menu Category by strongly typed.linq
@@ -1,7 +1,6 @@
-<Query Kind="Program">
+<Query Kind="Expression">
   <Connection>
     <ID>9291f6fb-c53e-4bc3-8660-9ae9c480f0cc</ID>
-    <Persist>true</Persist>
     <Server>.</Server>
     <Database>eRestaurant</Database>
   </Connection>
---
+++ eRestaurant-Sample/eRestaurant/BLL/ReservationController.cs
@@ -0,0 +1,40 @@
+﻿using eRestaurant.DAL;
+using eRestaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL
+{
+    [DataObject]
+    public class ReservationController
+    {
+
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SpecialEvent> ListAllReservations()
+        {
+            using (var context = new RestaurantContext())
+            {
+                return context.SpecialEvents.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Reservation> ListReservationsByEventCode(string EventCode)
+        {
+            using (var context = new RestaurantContext())
+            {
+                var result = from data in context.Reservations
+                           where data.EventCode == EventCode
+                           select data;
+
+                return result.ToList();
+            }
+        }
+
+    }
+}
