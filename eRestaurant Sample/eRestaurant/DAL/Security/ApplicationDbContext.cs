b5c8ac637ae498d4c5f5387d2783dcfc610c2419
rsingh785 <rsingh47@studentmail.nait.ca>
2014-11-18 14:42:43 -0700

    Edited

--- eRestaurant Sample/eRestaurant/DAL/Security/ApplicationDbContext.cs
+++ eRestaurant Sample/eRestaurant/DAL/Security/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace eRestaurant.DAL.Security
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+   internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection")
